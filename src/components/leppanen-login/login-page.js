ab877c544064f7785270016984845e1af504d11f
MiikaLepp <miikamleppanen@gmail.com>
2024-02-26 08:32:27 +0200

    feat: Adding a login page component, and i commented out stuff in app.js and index.js because they were causing errors with logo and such. Note: Had problems logging in to the test server, but could login on local strapi test server i made.

---
+++ src/components/leppanen-login/login-page.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const initialUser = { password: '', identifier: '' };
+
+const Login = () => {
+  const [user, setUser] = useState(initialUser);
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setUser((currentUser) => ({
+      ...currentUser,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = async () => {
+    const url = 'http://81.200.149.55:1337/admin/auth/login';
+    try {
+      if (user.identifier && user.password) {
+        const { data } = await axios.post(url, user);
+        if (data.jwt) {
+          alert('Login successful!');
+          setUser(initialUser);
+        } else {
+          alert('Invalid login credentials!');
+        }
+      }
+    } catch (error) {
+
+      console.error('Error during login:', error.message);
+
+      alert('Error during login. Please try again.');
+    }
+  };
+
+  return (
+    <div className='login'>
+      <div>
+        <h2>Login:</h2>
+        <label>
+          Email:
+          <input
+            type='email'
+            name='identifier'
+            value={user.identifier}
+            onChange={handleChange}
+            placeholder='Enter your email'
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type='password'
+            name='password'
+            value={user.password}
+            onChange={handleChange}
+            placeholder='Enter your password'
+          />
+        </label>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
