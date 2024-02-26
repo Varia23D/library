4cf146d97055cb1472232fbc5f8e208f05bc2655
MiikaLepp <miikamleppanen@gmail.com>
2024-02-26 11:16:35 +0200

    fix: there was a problem with a folder name in file structure, i changed it and forgot to save. Its now fixed.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './components/leppänen-login/login-page';
+import Login from './components/leppanen-login/login-page';
 import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
