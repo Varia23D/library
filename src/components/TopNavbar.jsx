37b7e29fed2422738cda4d30921379cba6b7fe60
Lohjelm <m.nea@hotmail.com>
2024-02-27 15:50:51 +0200

    Feat: Updated TopNavbar component for responsive design

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -10,10 +10,18 @@ const TopNavbar = () => {
 
   return (
     <div className="nav-container">
+      {/* Desktop Navbar */}
+      <ul className="desktop-menu">
+        <li><a className="menu__item" href="#">Edit Profile</a></li>
+        <li><a className="menu__item" href="#">Home</a></li>
+        <li><a className="menu__item" href="#">Books</a></li>
+        <li><a className="menu__item" href="#">Logout</a></li>
+      </ul>
+
+      {/* Mobile Navbar */}
       <div className="menu__btn" onClick={toggleMenu}>
         <span className={menuOpen ? 'open' : ''}></span>
       </div>
-
       <ul className={`menu__box ${menuOpen ? 'open' : ''}`}>
         <li className="user-profile">
           <img className="user-avatar" src="https://popcat.click/twitter-card.jpg" alt="User Avatar" />
