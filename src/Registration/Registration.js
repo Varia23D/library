70eee7426ed09a0a2d6eb86bcb10521b82c770b0
oliviaskye <oliviasky3@icloud.com>
2024-03-04 18:03:35 +0200

    fix: database link (currently using local link) and added comments. still needs link to redirect to login page after successful registration.

--- src/Registration/Registration.js
+++ src/Registration/Registration.js
@@ -2,32 +2,34 @@ import axios from 'axios';
 import React, {useState} from 'react';
 import './Registration.css';
 
-let user1 = {username:'', email:'', password:''};
+let user1 = {username:'', email:'', password:''};                  // blank initial user
 
 let Registration = () => {
 
-    let [user, setUser] = useState(user1);    
+    let [user, setUser] = useState(user1);                         // pass blank initial user to useState
 
     let register = async () => {
         try{
-            let url = 'http://localhost:1337/auth/local/register'; // database link http://81.200.149.55:1337/api/auth/register
-            if (user.username && user.email && user.password){
-                let result = await axios.post(url, user);
+            let url = 'http://localhost:1337/api/auth/local/register'; // (test database 'http://81.200.149.55:1337')
+            if (user.username && user.email && user.password){     // ensure all input fields have content
+                let result = await axios.post(url, user);          // make post request to API with user info
                 if (result){
-                    setUser(user1);
-                    console.log("registered successfully");
-                    // navigate to login page
+                    setUser(user1);                                // if the API responds, set input values to the user
+                    console.log('registered successfully');
+                    alert('registered successfully');
+                    // TO ADD: navigate to login page after successful registration
                 }
             }
         }
         catch(error){
-            console.error(error.message);
+            console.error(error.message);                           // display error message if registration is unsuccessful 
+            alert('Registration unsuccessful: ' + error.message);
         }
     };
 
-    let handleChange = ({target}) => {
-        let {name, value} = target;
-        setUser((currentUser) => ({...currentUser, [name]:value}));
+    let handleChange = ({target}) => {                              // function to handle input  
+        let {name, value} = target;                                 // target's name and value assigned to variables
+        setUser((currentUser) => ({...currentUser, [name]:value})); // assigns input value to the blank user with setUser hook
     };
 
     return(
