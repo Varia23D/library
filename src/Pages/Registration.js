10e355789fcd25826da539f444947284b7a4e9d7
oliviaskye <oliviasky3@icloud.com>
2024-03-15 10:14:32 +0200

    adjusted element naming for clarity

--- src/Pages/Registration.js
+++ src/Pages/Registration.js
@@ -8,22 +8,24 @@ const Registration = () => {
     const { user, handleChange, register } = registrationHandler();
 
     return (
-        <div className='main'>
-            <div className='title'>
-                <div className='registration'><h1>Registration</h1></div>
+        <div className='registration'>
+        <div className='registration_background_img'>
+            <div className='registration_heading'>
+                <h1>Registration</h1>
             </div>
-            <div className='input'>
+            <div className='registration_input'>
                 <label htmlFor='username' >Name</label> 
                 <input type='text' name='username' value={user.username} placeholder='Enter your name' onChange={handleChange}/>
                 <label htmlFor='email' >Email</label>
                 <input type='email' name='email' value={user.email} placeholder='Enter your email' onChange={handleChange}/>
                 <label htmlFor='password' >Password</label>
                 <input type='password' name='password' value={user.password} placeholder='Enter your password' onChange={handleChange}/>
-                <div className='buttonBox'> 
-                    <button className='registerButton' onClick={register}>register</button>
-                    <button className='moodleButton'>register with moodle</button>
+                <div className='registration_buttons'> 
+                    <button className='register_button' onClick={register}>register</button>
+                    <button className='moodle_button'>register with moodle</button>
                 </div>
             </div>
+        </div>
         </div>
     );
 };
