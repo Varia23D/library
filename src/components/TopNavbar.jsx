f18e5673895329b875e73d0ec86cbe9e0a593812
Lohjelm <m.nea@hotmail.com>
2024-03-21 12:07:55 +0200

    small visual changes

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,6 +1,5 @@
 // Q:? why we repeat links
 import { useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
 import React, { useState, useEffect } from 'react';
 import './TopNavbar.css';
 
