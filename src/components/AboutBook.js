f1ec52b8c7c87876167401936444cd3acd1d2a5f
Vinti <m.nea@hotmail.com>
2024-03-25 20:25:20 +0200

    feat: Added QR button

--- src/components/AboutBook.js
+++ src/components/AboutBook.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import './AboutBook.css';
+import QrReader from './QrReader';
 
-const BookPage = ({ book, startScanner, stopScanner }) => {
-
-  console.log('Book Details:', book.attributes.cover.data[0].attributes.url );
-
-  const toggleScanner = () => {
-    if (startScanner && stopScanner) {
-      startScanner();
-    } else if (stopScanner) {
-      stopScanner();
-    }
-  };
+const BookPage = ({ book,updateBooks}) => {
 
   return (
     <div className='whole-page-container'>
@@ -20,19 +11,17 @@ const BookPage = ({ book, startScanner, stopScanner }) => {
             <img className='circular-image' src={`${process.env.REACT_APP_BACKEND}${book.attributes.cover.data[0].attributes.url}`} alt="" />
           </div>
 
-              <div className='title-container'>
-                <span className='about-title'>{book.attributes.title}</span>
-              </div>
+          <div className='title-container'>
+            <span className='about-title'>{book.attributes.title}</span>
+          </div>
 
-                <div className='about-btn-container'>
-                    <button className='return-book-btn'onClick={toggleScanner}>
-                    <span className='btn-text'>Return book</span>
-                    </button>
-                  <div className='about-section'>
-                     <span className='about-the-book-title'>About</span>
-                     <span className='about-the-book-text'>{book.attributes.description}</span>
-                  </div>
-                </div>
+          <div className='about-btn-container'>
+          <QrReader updateBooks={updateBooks}/>
+            <div className='about-section'>
+              <span className='about-the-book-title'>About</span>
+              <span className='about-the-book-text'>{book.attributes.description}</span>
+            </div>
+          </div>
       </div>
     </div>
   );
