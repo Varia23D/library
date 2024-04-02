95451183c982954d09ced93c8d22fe3d4216a99e
arttunorja <arttu.norja@gmail.com>
2024-04-02 10:00:29 +0300

    feat: Remade toast notifications

--- src/helpers/fetchBookTypes.js
+++ src/helpers/fetchBookTypes.js
@@ -1,10 +1,12 @@
+import { toast } from 'react-toastify';   // Import toast
 
 export default function fetchBookTypes() {
   return fetch(`${process.env.REACT_APP_BACKEND}/api/book-types/?populate=*`)
-  .then(response => response.json())
-  .then(data => data.data)
-  .catch(error => {
-    console.error('Error fetching data:', error);
-    throw error
-  });
+    .then(response => response.json())
+    .then(data => data.data)
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      toast.error('Data fetch error. Please try again!');   // Toast error message
+      throw error
+    });
 };
