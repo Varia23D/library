35e98cc8557c16dc309718913594377524545ab8
arttunorja <arttu.norja@gmail.com>
2024-04-02 14:35:26 +0300

    Feat:Book search page with new changes

--- src/hooks/useBookSearchResults.jsx
+++ src/hooks/useBookSearchResults.jsx
@@ -23,28 +23,13 @@ const useBookSearchResults = (searchTerm) => {
             }
 
             try {
-                let bookTypesResponse = await axios.get(`${process.env.REACT_APP_BACKEND}/api/book-types?filters[title][$containsi]=${searchTerm}&populate=*`, {
+                const response = await axios.get(`${process.env.REACT_APP_BACKEND}/api/book-types?filters[title][$containsi]=${searchTerm}&populate=*`, {
                     headers: {
                         Authorization: `Bearer ${jwt}`,
                     },
                 });
 
-                const booksWithAvailability = await Promise.all(bookTypesResponse.data.data.map(async (bookType) => {
-                    const bookCopiesResponse = await axios.get(`${process.env.REACT_APP_BACKEND}/api/book-copies?filters[book_type][id][$eq]=${bookType.id}&populate=*`, {
-                        headers: {
-                            Authorization: `Bearer ${jwt}`,
-                        },
-                    });
-
-                    const isAvailable = bookCopiesResponse.data.data.some(copy => !copy.attributes.taken);
-
-                    return {
-                        ...bookType,
-                        isAvailable,
-                    };
-                }));
-
-                setBooks(booksWithAvailability);
+                setBooks(response.data.data);
             } catch (error) {
                 console.error('Error fetching books:', error);
                 setIsError(true);
