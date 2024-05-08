d3329474672b5c45b8b4a2d04008357ae2c91172
JyDan51 <yurkin2051@gmail.com>
2024-05-08 14:45:15 +0300

    gg
    gg

--- src/components/QrReader.jsx
+++ src/components/QrReader.jsx
@@ -2,18 +2,20 @@ import { Html5Qrcode } from "html5-qrcode";
 import { useState, useEffect, useRef } from "react";
 import { handleQRCodeScan } from "../helpers/QRCodeHandler";
 import '../css/QrReader.css'; // Import CSS file for styling
+import { Oval } from 'react-loader-spinner'
 
-const QrReader = ({updateBooks, isReturnButton,buttonText }) => {
+const QrReader = ({ updateBooks, isReturnButton, buttonText }) => {
   const html5QrCodeRef = useRef(null);
   const [isScannerRunning, setIsScannerRunning] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    //QR scaner init
+    // QR scanner init
     const html5QrCode = new Html5Qrcode('reader');
     html5QrCodeRef.current = html5QrCode;
 
     return () => {
-      //QR scanner stop 
+      // QR scanner stop 
       if (isScannerRunning) {
         html5QrCode.stop();
       }
@@ -27,7 +29,16 @@ const QrReader = ({updateBooks, isReturnButton,buttonText }) => {
     const qrCodeSuccessCallback = (decodedText) => {
       html5QrCode.stop();
       setIsScannerRunning(false);
-      handleQRCodeScan(decodedText, updateBooks);
+      // Delay execution of handleQRCodeScan by 2 seconds
+      setTimeout(() => {
+        handleQRCodeScan(decodedText, updateBooks);
+      }, 2000);
+      // Display modal window here
+      setShowModal(true);
+      // Hide modal after 2 seconds
+      setTimeout(() => {
+        setShowModal(false);
+      }, 2000);
     };
 
     const config = { fps: 10, aspectRatio: 1 };
@@ -48,20 +59,42 @@ const QrReader = ({updateBooks, isReturnButton,buttonText }) => {
     }
   };
 
+  // Function to close modal
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div>
-       <div id="reader" className={isScannerRunning ? 'scanner-open' : ''}></div>
+      <div id="reader" className={isScannerRunning ? 'scanner-open' : ''}></div>
       <div className={`qr-container`}>
-      {!isScannerRunning && (
+        {!isScannerRunning && (
           <button className="square-button" onClick={startScanner} > {buttonText ? buttonText : 'Scan QR code'}
           </button>
-      )}
-      {isScannerRunning && (
-        <button className="square-button" type="button" onClick={stopScanner} >
+        )}
+        {isScannerRunning && (
+          <button className="square-button" type="button" onClick={stopScanner} >
             Cancel
           </button>
-      )}
+        )}
       </div>
+
+      {/* Modal */}
+      {showModal && (
+        <div className="modal">
+          <div>
+            <div>
+              <div>
+                <Oval
+                  height={80}
+                  width={80}
+                  color="#84CCF8"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
