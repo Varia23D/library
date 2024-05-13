9ead341c435fe69e56d4382fe4c4d636427717a6
Lohjelm <142398812+Lohjelm@users.noreply.github.com>
2024-05-13 09:44:36 +0300

    QR button text logic fix (#62)
    * fix: QR button displays different title when going into the book details
    
    * Fix: fixed QR scanne spacing when scanner is open
    
    * Fix: Button text changes depending on book status on default on homepage / you dont own the book is: rent a book, when you own the book its:return book. Bookdetails title is now centered.
    
    * test api
    
    * feat: data fetch my transactions new function
    
    * fix: fixed button text logic
    
    * fix: deleted console log
    
    * Fix: QR button text logic is now changed from passing qr as a component to only passing a button and keeping over all same logic
    
    * Fix: QR button text is forced to show differently depending on page
    
    ---------
    
    Co-authored-by: Кирилл Бельков <plyushevo@gmail.com>

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -14,6 +14,7 @@ const BookDetails = ({ book }) => {
   }, [book, navigate]);
   
   const [buttonText, setButtonText] = useState(null);
+  
 
   useEffect(() => {
     console.log('Book details:', book);
