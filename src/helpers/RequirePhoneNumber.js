ecde6369cac1cb2d2c091f3bc501efcb7575cb88
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-05-29 00:52:59 +0300

    Prototyped component RequirePhoneNumber

---
+++ src/helpers/RequirePhoneNumber.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { userData } from '../helpers/userStorage';
+import axios from 'axios';
+
+const RequirePhoneNumber = ({ children }) => {
+	const navigate = useNavigate();
+	const currentUser = userData();
+
+	useEffect(() => {
+		const checkUserPhone = async () => {
+		try {
+			const response = await axios.get(`${process.env.REACT_APP_BACKEND}/api/users/me`, {
+			headers: {
+				Authorization: `Bearer ${currentUser.jwt}`,
+			},
+			});
+			if (!response.data.phone) {
+			navigate('/account-settings');
+			}
+		}
+		catch (error) {
+			console.error('Error fetching user data:', error);
+			navigate('/login');
+		}
+		};
+
+		if (currentUser.jwt) {
+		checkUserPhone();
+		}
+		else {
+		navigate('/login');
+		}
+	}, [navigate, currentUser]);
+
+	return children;
+};
+
+export default RequirePhoneNumber;
