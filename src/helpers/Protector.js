f8ba7fdca520b2fda0df865ee0dc95d2adbd2cb1
Rychkov Iurii <93578978+RychkovIurii@users.noreply.github.com>
2024-08-03 08:02:39 +0300

    Irychkov new feature/keycloak (#69)
    * Get access token
    
    * Add Keycloak redirect

--- src/helpers/Protector.js
+++ src/helpers/Protector.js
@@ -1,17 +1,17 @@
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-import { userData } from "./userStorage"
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { userData } from "./userStorage";
 
 // function takes component and redirects to login page if there is no jwt token in local storage 
 const Protector = ({children}) => {
-  const navigate = useNavigate()
-  const { jwt } = userData()
+  const navigate = useNavigate();
+  const { jwt } = userData();
+
   useEffect(() => {
     if (!jwt) {
-      navigate('/login')
+      navigate('/login');
     }
-  }, [navigate, jwt])
+  }, [navigate, jwt]);
 
   return jwt ? children : null;
 }
