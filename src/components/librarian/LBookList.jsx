61e65c39699cac3ec6df564ae03f1531d39864ea
Plyushevo <Plyushevo@gmail.com>
2024-08-05 13:55:49 +0300

    added pagination icons

--- src/components/librarian/LBookList.jsx
+++ src/components/librarian/LBookList.jsx
@@ -26,23 +26,23 @@ const LBookList = ({ tab }) => {
       const books = data.data
 
       //filter books by tab taken/overdue
-      let filteredBooks 
+      let filteredBooks
       if (tab === 'overdue') {
         const currentDate = new Date()
-        filteredBooks = books.filter((book) => book.attributes.transactions.data.some(transaction => 
-          transaction.attributes.open === true && 
+        filteredBooks = books.filter((book) => book.attributes.transactions.data.some(transaction =>
+          transaction.attributes.open === true &&
           new Date(transaction.attributes.returnDate) < currentDate
         ));
-      } else if ( tab === 'taken') {
+      } else if (tab === 'taken') {
         filteredBooks = books
       }
-      
-      
+
+
       setBooks(filteredBooks)
       setTotalPage(data.meta.pagination.pageCount)
     } catch (error) {
       console.error('Error getting transaction data:', error);
-        throw error;
+      throw error;
     } finally {
       setLoading(false)
     }
@@ -75,19 +75,24 @@ const LBookList = ({ tab }) => {
           <p>{tab === 'overdue' ? 'There are no overdue books' : 'There are no taken books'}</p>
         ) : (
           <>
-          {books.map((book) => (
-            <LBookItem key={book.id} book={book}/>
-          ))}
-          {totalPages > 1 && (
-          <div className="pagination-controls">
-            <button onClick={handlePrevPage} disabled={currentPage === 1}>
-            Previous
-            </button>
-            <span>Page {currentPage} from {totalPages}</span>
-            <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-              Next
-            </button>
-          </div>)}
+            {books.map((book) => (
+              <LBookItem key={book.id} book={book} />
+            ))}
+            {totalPages > 1 && (
+              <div className="pagination-controls">
+                <button onClick={handlePrevPage} disabled={currentPage === 1}>
+                  <svg width="9" height="18" viewBox="0 0 9 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7.99984 16.92L1.47984 10.4C0.709844 9.62996 0.709844 8.36996 1.47984 7.59996L7.99984 1.07996" stroke="#212529" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                </button>
+                <span>Page {currentPage} from {totalPages}</span>
+                <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+                  <svg width="9" height="18" viewBox="0 0 9 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.910156 16.92L7.43016 10.4C8.20016 9.62996 8.20016 8.36996 7.43016 7.59996L0.910156 1.07996" stroke="#212529" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+
+                </button>
+              </div>)}
           </>
         )}
     </div>
